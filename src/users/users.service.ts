3061b491a86c6740b916303b82b131d7e7f29997
JUNGganzi <wjdwjd1501@gmail.com>
2021-07-22 12:00:00 +0900

    슬랙 클론코딩 강의중

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepositoy: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepositoy: Repository<ChannelMembers>,
   ) {}
 
   async join(email: string, nickname: string, password: string) {
@@ -17,10 +23,19 @@ export class UsersService {
       throw new HttpException('이미 존재하는 이메일', 401);
     }
     const hashedPassword = await bcrypt.hash(password, 12);
-    await this.usersRepository.save({
+    const returned = await this.usersRepository.save({
       email,
       nickname,
       password: hashedPassword,
     });
+    await this.workspaceMembersRepositoy.save({
+      UserId: returned.id,
+      WorkspaceId: 1,
+    });
+    await this.channelMembersRepositoy.save({
+      UserId: returned.id,
+      ChannelId: 1,
+    });
+    return true;
   }
 }
