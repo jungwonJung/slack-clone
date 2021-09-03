cba07e9dddfb0cbc09900f505aa5ab8c5d2f012b
JUNGganzi <wjdwjd1501@gmail.com>
2021-09-03 12:00:00 +0900

    e2e test 관련 error 및 테스트

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
+import session from 'express-session';
+import passport from 'passport';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -12,6 +14,18 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.use(
+      session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+          httpOnly: true,
+        },
+      }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session()); // 사용하는 middleware 들도 다
     await app.init();
   });
 
@@ -20,5 +34,15 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/api/user/login (POST)', () => {
+    return request(app.getHttpServer()) // 테스트용 db 를 하나 만들기
+      .post('/api/user/login')
+      .send({
+        email: 'wjdwjd1501@gmail.com',
+        password: 123123,
+      })
+      .expect(201, done); // 비동기테스트는 작성해줘야한다고 함 done 을
   });
 });
